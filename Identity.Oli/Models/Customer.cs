bed911cacb95ee2aca2f853baca7ade7207514f9
OliLoftus <oliverloftus1@gmail.com>
2024-11-22 14:25:54 +0000

    Added validation to create and update operations

--- Identity.Oli/Models/Customer.cs
+++ Identity.Oli/Models/Customer.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Identity.Oli.Models;
 
 public class Customer
 {
     public Guid Id { get; set; }
+    
+    [Required] // Name field cannot be null or empty.
+    [StringLength(50, MinimumLength = 2)] // Limits length of name field.
     public string Name { get; set; }
+    
+    [Required]
+    [EmailAddress] // Ensure valid email address
     public string Email { get; set; }
 }
